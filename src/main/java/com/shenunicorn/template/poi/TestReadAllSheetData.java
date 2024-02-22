054e2af9d2da40097ec5a1d482ab26b1decc2389
Unicorn Liu <shen.unicorn@hotmail.com>
2024-02-22 12:17:48 +0800

    [create] TestReadAllSheetData

--- src/main/java/com/shenunicorn/template/poi/TestReadAllSheetData.java
+++ src/main/java/com/shenunicorn/template/poi/TestReadAllSheetData.java
@@ -1,8 +1,13 @@
 package com.shenunicorn.template.poi;
 
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFCell;
@@ -10,26 +15,65 @@ import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
+import com.shenunicorn.template.poi.vo.TrunRow;
+
 public class TestReadAllSheetData {
 
 	public static void main(String[] args) {
-		try (FileInputStream fis = new FileInputStream("C:\\Work\\TestReadAllSheetData.xlsx");) {
+		try (FileInputStream fis = new FileInputStream("C:\\Work\\TestReadAllSheetData.xlsx");
+				XSSFWorkbook workbook = new XSSFWorkbook(fis);) {
 
-			XSSFWorkbook workbook = new XSSFWorkbook(fis);
 			int numberOfSheets = workbook.getNumberOfSheets();
 
-			for (int i = 0; i < numberOfSheets; i++) {
-				XSSFSheet sheet = workbook.getSheetAt(i);
+			for (int sheetIndex = 0; sheetIndex < numberOfSheets; sheetIndex++) {
+				XSSFSheet sheet = workbook.getSheetAt(sheetIndex);
+				System.out.println("sheet.getSheetName() = " + sheet.getSheetName());
+
+				final List<TrunRow> rowList = new ArrayList<>();
 
 				Iterator<Row> rowsIterator = sheet.iterator();
 				while (rowsIterator.hasNext()) {
 					XSSFRow row = (XSSFRow) rowsIterator.next();
+					int rowIndex = row.getRowNum();
+
+					final TrunRow rowData = new TrunRow();
+					if (rowIndex == 0) {
+						rowData.setValueType("H");
+					} else {
+						rowData.setValueType("D");
+					}
+					
+					final StringBuilder jsonValue = new StringBuilder();
+					jsonValue.append("[");
 
 					Iterator<Cell> cellsIterator = row.cellIterator();
 					while (cellsIterator.hasNext()) {
 						XSSFCell cell = (XSSFCell) cellsIterator.next();
-						System.out.println("cell = " + cell.getRawValue() + ":" + cell.toString());
+						int cellIndex = cell.getColumnIndex();
+						System.out.println("numberOfSheets: " + sheetIndex + " rowIndex: " + rowIndex + " cellIndex: "
+								+ cellIndex + " cellValue: " + cell.toString());
+
+						final String value = cell.toString();
+						if (cellIndex == 0) {
+							rowData.setAreaName(value);
+						} else if (cellIndex == 1) {
+							rowData.setBranchName(value);
+						} else if (cellIndex == 2) {
+							rowData.setEmpId(value);
+						} else if (cellIndex == 2) {
+							rowData.setCustId(value);
+						} else {
+							jsonValue.append(",");
+							jsonValue.append(cell.toString());
+						}
 					}
+					jsonValue.append("]");
+					rowData.setJsonValue(StringUtils.replaceOnce(jsonValue.toString(), ",", ""));
+					rowList.add(rowData);
+				}
+				for (TrunRow row : rowList) {
+					System.out.println("numberOfSheets: " + sheetIndex + " SheetName: " + sheet.getSheetName() + " " + ToStringBuilder.reflectionToString(row, ToStringStyle.MULTI_LINE_STYLE));
+					
 				}
 			}
 
@@ -37,4 +81,5 @@ public class TestReadAllSheetData {
 			e.printStackTrace();
 		}
 	}
+
 }
---
+++ src/main/java/com/shenunicorn/template/poi/vo/TrunRow.java
@@ -0,0 +1,46 @@
+package com.shenunicorn.template.poi.vo;
+
+public class TrunRow {
+	private String areaName;
+	private String branchName;
+	private String empId;
+	private String custId;
+	private String jsonValue;
+	private String valueType;
+	public String getAreaName() {
+		return areaName;
+	}
+	public void setAreaName(String areaName) {
+		this.areaName = areaName;
+	}
+	public String getBranchName() {
+		return branchName;
+	}
+	public void setBranchName(String branchName) {
+		this.branchName = branchName;
+	}
+	public String getEmpId() {
+		return empId;
+	}
+	public void setEmpId(String empId) {
+		this.empId = empId;
+	}
+	public String getCustId() {
+		return custId;
+	}
+	public void setCustId(String custId) {
+		this.custId = custId;
+	}
+	public String getJsonValue() {
+		return jsonValue;
+	}
+	public void setJsonValue(String jsonValue) {
+		this.jsonValue = jsonValue;
+	}
+	public String getValueType() {
+		return valueType;
+	}
+	public void setValueType(String valueType) {
+		this.valueType = valueType;
+	}
+}
